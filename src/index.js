fb35539406a9e8d33d71e46e1df58926e77cc942
Kan1shak <112748208+Kan1shak@users.noreply.github.com>
2023-07-13 23:55:39 +0530

    Set up nav

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+const createNav = () => {
+    const navbar = document.createElement('nav');
+
+    const home = document.createElement('a');
+    const menu = document.createElement('a');
+    const contact = document.createElement('a');
+
+    home.textContent = 'Home'
+    menu.textContent = 'Menu';
+    contact.textContent = 'Contact';
+
+    navbar.appendChild(home);
+    navbar.appendChild(menu);
+    navbar.appendChild(contact);
+    document.querySelector('#content').appendChild(navbar);
+
+    document.querySelectorAll('nav > a').forEach((item)=> {
+        item.addEventListener('click', () =>{
+            console.log('yes!');
+        })
+    })
+}
+
+createNav();
